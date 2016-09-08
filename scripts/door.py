a5418f35f4c2af8fb191d211c5076bcecbba34b8
hsinewu <extraordinary1022@gmail.com>
2016-09-08 15:10:59 +0800

    argparse map type

--- scripts/door.py
+++ scripts/door.py
@@ -1,26 +1,41 @@
 #!/home/hsinewu/anaconda3/bin/python
+import argparse
+argp = argparse.ArgumentParser()
+argp.add_argument( "time", help="specify the time of the day", type=int, choices=range(4))
+argp.add_argument( "height", help="specify the height level", type=int, choices=range(16))
+argp.add_argument( "kind", help="specify what kind of map to draw", choices=['pc', 'cnt', 'cntf'])
+args = argp.parse_args()
+print(args)
+
 import numpy as np
 from scipy.io.netcdf import netcdf_file
 from mpl_toolkits.basemap import Basemap
-import sys
+
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-if len(sys.argv) != 3 :
-	print( "Hsine is expecting len(argv) == 3)")
-	exit()
-time, height = int( sys.argv[1]), int( sys.argv[2])
-
 with netcdf_file('ph1.nc', 'r') as f:
 	lats = f.variables['lat'][:]
 	lons = f.variables['lon'][:]
-	data = f.variables['t'][time, height]
+	data = f.variables['t'][args.time, args.height]
 
+fig = plt.figure(figsize=(15.36,7.68))
 map = Basemap(resolution='l',lon_0=180)
 xx, yy = np.meshgrid(lons, lats)
-map.pcolormesh(xx, yy, np.squeeze(data))
-map.drawcoastlines()
-map.drawcountries()
-plt.savefig('output.png')
+
+if args.kind == 'pc':
+	map.pcolormesh(xx, yy, np.squeeze(data))
+elif args.kind == 'cnt':
+	map.contour(xx, yy, np.squeeze(data))
+elif args.kind == 'cntf':
+	map.contourf(xx, yy, np.squeeze(data))
+
+fig.tight_layout()
+fig.subplots_adjust(bottom = 0)
+fig.subplots_adjust(top = 1)
+fig.subplots_adjust(right = 1)
+fig.subplots_adjust(left = 0)
+plt.axis('off')
+plt.savefig('output.png', bbox_inches='tight', pad_inches=0)
