7f13e3eb568eb3a46334bf63f06242a146fd8fbf
hsinewu <extraordinary1022@gmail.com>
2016-11-01 14:04:45 +0800

    delete and rename scripts

--- scripts/pillow/palette.py
+++ scripts/pillow/palette.py
@@ -2,6 +2,7 @@ from scipy.io.netcdf import netcdf_file
 from PIL import Image
 from json import loads as json_loads
 from bisect import bisect_left
+from numpy import interp
 
 # read netcdf
 with netcdf_file('r85.nc', 'r') as f:
@@ -14,17 +15,19 @@ with open("heatColor.json") as f2:
 	heatColor = json_loads(f2.read())
 
 # draw png in palette mode with pillow
-size = data.shape
-im = Image.new("P", (size[1], size[0]))
+size = (512,256)
+dsize = data.shape
+im = Image.new("P", size)
 im.putpalette(heatColor['palette'])
 
-for lt,ln in [(lat,lon) for lat in range(size[0]) for lon in range(size[1])]:
-	da = data[lt,ln] - 273.15 # K to Celsius
+for px,py in [(x,y) for x in range(size[0]) for y in range(size[1])]:
+	ln, lt = interp(px, [0, size[0]], [0, dsize[1]]), interp(py, [0, size[1]], [0, dsize[0]])
+	da = data[int(lt), int(ln)] - 273.15 # K to Celsius
 	# if data value is higher than specified color range, give it last color
 	if da >= heatColor['stops'][-1]:
 		stop_index = len(heatColor['palette']) - 1
 	else:
 		stop_index = bisect_left( heatColor['stops'], da)
-	im.putpixel((ln,lt), stop_index)
+	im.putpixel((px,py), stop_index)
 
-im.save("_basic.png")
+im.save("_scale.png")
--- scripts/pillow/scale_near.py
+++ scripts/pillow/scale_near.py
@@ -4,14 +4,17 @@ from json import loads as json_loads
 from bisect import bisect_left
 from numpy import interp
 
+ncFile = 'r85.nc'
+vizItem = 'temp2'
+colorFile = 'heatColor.json'
 # read netcdf
-with netcdf_file('r85.nc', 'r') as f:
+with netcdf_file(ncFile, 'r') as f:
 	lats = f.variables['lat'][:]
 	lons = f.variables['lon'][:]
-	data = f.variables['temp2'][0] # (time, lat, lon)
+	data = f.variables[vizItem][0] # (time, lat, lon)
 
 # read color data
-with open("heatColor.json") as f2:
+with open(colorFile) as f2:
 	heatColor = json_loads(f2.read())
 
 # draw png in palette mode with pillow
