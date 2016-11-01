36824cdb4c69ae2c42e8295bf597cd1e9201a752
hsinewu <extraordinary1022@gmail.com>
2016-11-01 14:18:52 +0800

    refact palette printPng

--- scripts/pillow/palette.py
+++ scripts/pillow/palette.py
@@ -13,7 +13,7 @@ outputFile = '_scale.png'
 with netcdf_file(ncFile, 'r') as f:
 	lats = f.variables['lat'][:]
 	lons = f.variables['lon'][:]
-	data = f.variables[vizItem][0] # (time, lat, lon)
+	data = f.variables[vizItem] # (time, lat, lon)
 
 # read color data
 with open(colorFile) as f2:
@@ -25,14 +25,16 @@ dsize = data.shape
 im = Image.new("P", size)
 im.putpalette(colorTable['palette'])
 
-for px,py in [(x,y) for x in range(size[0]) for y in range(size[1])]:
-	ln, lt = interp(px, [0, size[0]], [0, dsize[1]]), interp(py, [0, size[1]], [0, dsize[0]])
-	da = data[int(lt), int(ln)] - 273.15 # K to Celsius
-	# if data value is higher than specified color range, give it last color
-	if da >= colorTable['stops'][-1]:
-		stop_index = len(colorTable['palette']) - 1
-	else:
-		stop_index = bisect_left( colorTable['stops'], da)
-	im.putpixel((px,py), stop_index)
+def printPng(space, transform=lambda x:x):
+	for px,py in [(x,y) for x in range(size[0]) for y in range(size[1])]:
+		ln, lt = interp(px, [0, size[0]], [0, dsize[1]]), interp(py, [0, size[1]], [0, dsize[0]])
+		da = transform( data[int(lt), int(ln)])
+		# if data value is higher than specified color range, give it last color
+		if da >= colorTable['stops'][-1]:
+			stop_index = len(colorTable['palette']) - 1
+		else:
+			stop_index = bisect_left( colorTable['stops'], da)
+		im.putpixel((px,py), stop_index)
 
+printPng(data[0], lambda x: x-273.15)
 im.save(outputFile)
