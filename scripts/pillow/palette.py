43dceaa6046f80c8955a1de487be7e24516e2cf8
hsinewu <extraordinary1022@gmail.com>
2016-11-01 14:11:21 +0800

    refact palette extract string

--- scripts/pillow/palette.py
+++ scripts/pillow/palette.py
@@ -4,30 +4,35 @@ from json import loads as json_loads
 from bisect import bisect_left
 from numpy import interp
 
+ncFile = 'r85.nc'
+vizItem = 'temp2'
+colorFile = 'heatColor.json'
+outputFile = '_scale.png'
+
 # read netcdf
-with netcdf_file('r85.nc', 'r') as f:
+with netcdf_file(ncFile, 'r') as f:
 	lats = f.variables['lat'][:]
 	lons = f.variables['lon'][:]
-	data = f.variables['temp2'][0] # (time, lat, lon)
+	data = f.variables[vizItem][0] # (time, lat, lon)
 
 # read color data
-with open("heatColor.json") as f2:
-	heatColor = json_loads(f2.read())
+with open(colorFile) as f2:
+	colorTable = json_loads(f2.read())
 
 # draw png in palette mode with pillow
 size = (512,256)
 dsize = data.shape
 im = Image.new("P", size)
-im.putpalette(heatColor['palette'])
+im.putpalette(colorTable['palette'])
 
 for px,py in [(x,y) for x in range(size[0]) for y in range(size[1])]:
 	ln, lt = interp(px, [0, size[0]], [0, dsize[1]]), interp(py, [0, size[1]], [0, dsize[0]])
 	da = data[int(lt), int(ln)] - 273.15 # K to Celsius
 	# if data value is higher than specified color range, give it last color
-	if da >= heatColor['stops'][-1]:
-		stop_index = len(heatColor['palette']) - 1
+	if da >= colorTable['stops'][-1]:
+		stop_index = len(colorTable['palette']) - 1
 	else:
-		stop_index = bisect_left( heatColor['stops'], da)
+		stop_index = bisect_left( colorTable['stops'], da)
 	im.putpixel((px,py), stop_index)
 
-im.save("_scale.png")
+im.save(outputFile)
