c9b1b312a67dd3be0d6f6d5a4a600ac815d93453
hsinewu <extraordinary1022@gmail.com>
2016-12-08 10:04:36 +0800

    :laughing: printPng from database

--- scripts/pillow/maria.py
+++ scripts/pillow/maria.py
@@ -1,6 +1,27 @@
 from yaml import load as yaml_load
+from math import sqrt
+import mysql.connector
+import numpy as np
+from palette import pringPng
 
 with open('database.yaml') as f:
 	db_cfg = yaml_load(f.read())
 
-print(db_cfg)
+try:
+	print("Connect to database...")
+	db = mysql.connector.connect(**db_cfg)
+	cursor = db.cursor(buffered=True)
+	print("Execute query...")
+	cursor.execute("select olr from %s;")
+	data = cursor.fetchall()
+	w = int( sqrt( len(data)/2))
+	data = np.reshape( data, (w, w*2))
+
+	print("Generate png...")
+	printPng( data, "olr.png")
+	db.close()
+	print("Database closed successfully")
+
+except mysql.connector.Error as err:
+	print("Error occurs upon connection: ")
+	print(err.msg)
