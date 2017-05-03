ba8a69e9be007eb861acd9e26d31967cef38b6a8
hsinewu <extraordinary1022@gmail.com>
2017-05-03 09:59:03 +0800

    +form.php var_dump, bootstrap 3

---
+++ form.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Upload netcdf</title>
+  <!-- bootstrap -->
+  <link rel="stylesheet" href="css/bootstrap.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <script src="js/bootstrap.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+  <div class="container">
+      <div class="col-md-6 col-md-offset-3">
+    <br><br> <!-- Orz -->
+    <h1>Upload your netcdf</h1>
+    <form method="post" enctype="multipart/form-data">
+      <div class="form-group">
+        <label><h3>File Name:</h3></label>
+        <input class="form-control-file" name="nc" accept=".nc" required="" type="file">
+      </div>
+      <div class="form-group">
+        <label><h3>Start date:</h3></label>
+        <input class="form-control" name="date" placeholder="2016-12-01" required="" type="date">
+      </div>
+      <div class="form-group">
+        <label><h3>Variable names:</h3></label>
+        <input class="form-control" name="vars" placeholder="var1, var2" required="" type="text">(in csv)
+      </div>
+      <div class="form-group">
+        <label><h3>Length of each record:</h3></label>
+        <input class="form-control" name="length" placeholder="180" required="" type="text">
+      </div>
+      <div class="form-group">
+        <label><h3>Interval:</h3></label>
+        <input class="form-control" name="interval" min="1" value="6" required="" type="number"> (in hours)
+      </div>
+      <button class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+  </div>
+
+</body></html>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'GET')
+  exit();
+    $len = $_POST['length'];
+    $vars = $_POST['vars'];
+    $vars = array_map('trim', str_getcsv($vars));
+    $wsv = implode(" ", $vars);
+    $dh = $_POST['interval'];
+    $date = $_POST['date'];
+    $date = str_replace('-', '', $date)."00";
+    $schema = ['names' => [$date], 'items' => $vars, 'dh' => $dh, 'length' => $len];
+    var_dump($schema);
